842d5f6057f7456de91ff1703de27f55cf34580d
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-03-16 17:22:39 -0400

    feat(footer): add questionnaire to send question

--- src/app/pages/froggy-game/froggy-game.module.ts
+++ src/app/pages/froggy-game/froggy-game.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { FroggyGameRoutingModule } from './froggy-game-routing.module';
 import { FroggyGameComponent } from './froggy-game.component';
 import { IonicModule } from '@ionic/angular';
+import { FooterModule } from 'src/app/components/footer/footer.module';
 
 @NgModule({
   declarations: [FroggyGameComponent],
-  imports: [CommonModule, FroggyGameRoutingModule, IonicModule],
+  imports: [CommonModule, FroggyGameRoutingModule, IonicModule, FooterModule],
 })
 export class FroggyGameModule {}
--- src/app/pages/froggy-game/privacy-policy/privacy-policy.module.ts
+++ src/app/pages/froggy-game/privacy-policy/privacy-policy.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { PrivacyPolicyRoutingModule } from './privacy-policy-routing.module';
 import { PrivacyPolicyComponent } from './privacy-policy.component';
 import { IonicModule } from '@ionic/angular';
+import { FooterModule } from 'src/app/components/footer/footer.module';
 
 @NgModule({
   declarations: [PrivacyPolicyComponent],
-  imports: [CommonModule, PrivacyPolicyRoutingModule, IonicModule],
+  imports: [
+    CommonModule,
+    PrivacyPolicyRoutingModule,
+    IonicModule,
+    FooterModule,
+  ],
 })
 export class PrivacyPolicyModule {}
